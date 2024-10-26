d87bbf44c7227f27a177939e4147c0d4032ee45a
Anubhav Jain <anubhav27nov@gmail.com>
2024-10-26 19:44:02 +0530

    Intialize

---
+++ src/components/ChannelContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelContainer = () => {
+    return (
+        <div>
+            <p>Channel Container</p>
+        </div>
+    );
+};
+
+export default ChannelContainer;
---
+++ src/components/ChannelListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelListContainer = () => {
+    return (
+        <div>
+            <p>Channel List Container</p>
+        </div>
+    );
+};
+
+export default ChannelListContainer;
