d9fcc6af015d4e949da40a307157bc559fc3c784
Anubhav Jain <anubhav27nov@gmail.com>
2024-10-27 18:29:23 +0530

    Implemented SignIn Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
 import './App.css';
 
@@ -10,7 +11,11 @@ const apiKey = "6nvp7fs58bk6";
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+
+    if (!authToken) return <Auth />
     return (
         <div className="app_wrapper">
             <Chat client={client} theme="team light">
