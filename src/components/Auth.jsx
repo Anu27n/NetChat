9f12777b5ac5fe4b182970c5d79b56292cbdab02
Anubhav Jain <anubhav27nov@gmail.com>
2024-10-28 00:01:07 +0530

    Created Sign Up Form

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -15,17 +15,22 @@ const initialState = {
 const Auth = () => {
     const [form, setForm] = useState(initialState); 
     const [isSignup, setIsSignup] = useState(true);
+    
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
+    }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
         console.log(form);
     }
+
     return (
         <div className="auth__form-container">
             <div className="auth__form-container_fields">
                 <div className="auth__form-container_fields-content">
                     <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
-                    <form onSubmit={() => {}}>
+                    <form onSubmit={handleSubmit}>
                         {isSignup && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="fullName">Full Name</label>
@@ -107,7 +112,7 @@ const Auth = () => {
                         <p>
                             {isSignup ? 'Already have an account?' : "Don't have an account?"}
                             <span onClick={() => setIsSignup((prevIsSignup) => !prevIsSignup)}>
-                                {isSignup ? 'Sign Up' : 'Sign In'}
+                                {isSignup ? 'Sign In' : 'Sign Up'}
                             </span>
                         </p>
                     </div>
@@ -115,7 +120,6 @@ const Auth = () => {
             </div>
             <div className="auth__form-container_image">
                 <img src={signinImage} alt="sign in" />
-
             </div>
         </div>
     );
